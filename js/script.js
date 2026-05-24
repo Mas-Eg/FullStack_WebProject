3c2f3c41ea8fa5c4f2893008efdc4b5e6c12138b
Mas-Eg <georgiamaniks@gmail.com>
2026-05-24 22:58:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -222,7 +222,7 @@ if (contactForm) {
         showFieldErrors(null); // очищаем ошибки
 
         try {
-            const response = await fetch('/api', {
+            const response = await fetch('/api/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
