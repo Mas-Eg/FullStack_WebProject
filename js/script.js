f2191ebcd2d90a064766de108ba6251b5a9ab438
Mas-Eg <georgiamaniks@gmail.com>
2026-05-25 09:53:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -285,7 +285,7 @@ async function checkAuth() {
 
 async function loadUserProfile(userId) {
     try {
-        const resp = await fetch(API_BASE+'/{userId}`, {
+        const resp = await fetch(API_BASE+'/{userId}', {
             headers: { 'X-Requested-With': 'XMLHttpRequest' }
         });
         if (resp.ok) {
